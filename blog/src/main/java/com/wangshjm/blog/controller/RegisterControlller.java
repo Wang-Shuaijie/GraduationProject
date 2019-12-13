d13a802ef138e05a489decaa785a43b7a7f5b791
WangShuaijie <2283613941@qq.com>
2019-12-13 16:06:28 +0800

    注册controller

--- blog/src/main/java/com/wangshjm/blog/controller/RegisterControlller.java
+++ blog/src/main/java/com/wangshjm/blog/controller/RegisterControlller.java
@@ -1,4 +1,185 @@
 package com.wangshjm.blog.controller;
 
-public class RegisterControlller {
+import com.wangshjm.blog.entity.User;
+import com.wangshjm.blog.service.UserService;
+import com.wangshjm.blog.utils.MD5Util;
+import com.wangshjm.blog.utils.MailUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+@Slf4j
+public class RegisterControlller extends BaseController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @RequestMapping("/doRegister")
+    public String doRegister(Model model, @RequestParam(value = "email", required = false) String email,
+                             @RequestParam(value = "password", required = false) String password,
+                             @RequestParam(value = "phone", required = false) String phone,
+                             @RequestParam(value = "nickName", required = false) String nickname,
+                             @RequestParam(value = "code", required = false) String code) {
+        log.debug("注册");
+        if (StringUtils.isEmpty(code)) {
+            model.addAttribute("error", "非法注册，请重新注册！");
+            return "register";
+        }
+
+        int b = checkValidateCode(code);
+        if (b == -1) {
+            model.addAttribute("error", "验证码超时，请重新注册！");
+            return "register";
+        } else if (b == 0) {
+            model.addAttribute("error", "验证码不正确,请重新输入!");
+            return "register";
+        }
+
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            model.addAttribute("error", "该用户已经被注册！");
+            return "register";
+        } else {
+            user = new User();
+            user.setNickName(nickname);
+
+            user.setPassword(MD5Util.encodeToHex("salt" + password));
+            user.setPhone(phone);
+            user.setEmail(email);
+            user.setState("0");
+            user.setEnable("0");
+            user.setImgUrl("/images/icon_m.jpg");
+            //生成邮件激活码
+            String validateCode = MD5Util.encodeToHex("salt" + email + password);
+            redisTemplate.opsForValue().set(email, validateCode, 24, TimeUnit.HOURS);// 24小时 有效激活 redis保存激活码
+
+            userService.regist(user);
+            log.info("注册成功");
+            //发送激活邮件
+            MailUtil.sendEmailMessage(email, validateCode);
+            String message = email + "," + validateCode;
+            model.addAttribute("message", message);
+            return "regist/registerSuccess";
+        }
+    }
+
+    /**
+     * 判断手机号是否已经被注册
+     *
+     * @param model
+     * @param phone
+     * @return
+     */
+    @RequestMapping("/checkPhone")
+    @ResponseBody
+    public Map<String, Object> checkPhone(Model model, @RequestParam(value = "phone", required = false) String phone) {
+        log.debug("注册-判断手机号" + phone + "是否可用");
+        Map map = new HashMap<String, Object>();
+        User user = userService.findByPhone(phone);
+        if (user == null) {
+            //未注册
+            map.put("message", "success");
+        } else {
+            //已注册
+            map.put("message", "fail");
+        }
+        return map;
+    }
+
+
+    /**
+     * 判断邮箱是否可用
+     *
+     * @param model
+     * @param email
+     * @return
+     */
+    @RequestMapping("/checkEmail")
+    @ResponseBody
+    public Map<String, Object> checkEmail(Model model, @RequestParam(value = "email", required = false) String email) {
+        log.debug("注册-判断邮箱" + email + "是否可用");
+        Map map = new HashMap<String, Object>();
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            //未注册
+            map.put("message", "success");
+        } else {
+            //已注册
+            map.put("message", "fail");
+        }
+        return map;
+    }
+
+    @RequestMapping("/activecode")
+    public String active(Model model) {
+        log.info("==============激活验证==================");
+        //判断激活码有无过期 是否正确
+        String validateCode = getRequest().getParameter("validateCode");
+        String email = getRequest().getParameter("email");
+        String code = redisTemplate.opsForValue().get(email);
+        log.info("验证邮箱为：" + email + ",邮箱激活码为：" + code + ",用户链接的激活码为：" + validateCode);
+        //判断是否已激活
+        User userTrue = userService.findByEmail(email);
+        if (userTrue != null && "1".equals(userTrue.getState())) {
+            //已激活
+            model.addAttribute("success", "您已激活,请直接登录！");
+            return "/login";
+        }
+        if (code == null) {
+            //激活码过期
+            model.addAttribute("fail", "您的激活码已过期,请重新注册！");
+            userService.deleteByEmail(email);
+            return "/regist/activeFail";
+        }
+
+        if (StringUtils.isEmpty(validateCode) && validateCode.equals(code)) {
+            //激活码正确
+            userTrue.setEnable("1");
+            userTrue.setState("1");
+            userService.update(userTrue);
+            model.addAttribute("email", email);
+            return "/regist/activeSuccess";
+        } else {
+            //激活码错误
+            model.addAttribute("fail", "您的激活码错误,请重新激活！");
+            return "/regist/activeFail";
+        }
+    }
+
+
+    /**
+     * 检查验证码的正确性
+     *
+     * @param code
+     * @return
+     */
+    private int checkValidateCode(String code) {
+        Object vercode = getSession().getAttribute("VERCODE_KEY");
+        if (null == vercode) {
+            return -1;
+        }
+        if (!code.equalsIgnoreCase(vercode.toString())) {
+            return 0;
+        }
+        return 1;
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model) {
+        log.info("进入注册页面");
+        return "/register";
+    }
+
 }
