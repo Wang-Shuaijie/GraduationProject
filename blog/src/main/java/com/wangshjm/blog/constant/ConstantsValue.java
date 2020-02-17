b14a15f3448d9bbdd35a972bd768b54dc7ce4a33
WangShuaijie <2283613941@qq.com>
2020-02-17 15:00:51 +0800

    常量类更新（后期可能直接删除）

--- blog/src/main/java/com/wangshjm/blog/constant/ConstantsValue.java
+++ blog/src/main/java/com/wangshjm/blog/constant/ConstantsValue.java
@@ -4,5 +4,6 @@ package com.wangshjm.blog.constant;
  * 项目常量类
  */
 public class ConstantsValue {
-    public static final String SALT="salt";
+    //加密，解密密钥
+    public static final String SALT = "wangshjm";
 }
