2df4f68ea7fbf9bee15250e44fd0c7cf771c6831
WangShuaijie <2283613941@qq.com>
2020-02-17 15:01:35 +0800

    监听消息类更新

--- blog/src/main/java/com/wangshjm/blog/activemq/MqConsumer.java
+++ blog/src/main/java/com/wangshjm/blog/activemq/MqConsumer.java
@@ -15,14 +15,14 @@ import javax.jms.Message;
 @Slf4j
 public class MqConsumer {
     @JmsListener(destination = "login_msg")
-    public void receiveMessage(Message message){
+    public void receiveMessage(Message message) {
         log.info("监听到消息！");
         MapMessage mapMessage = (MapMessage) message;
         // 调用SMS服务发送短信   SmsSystem阿里大于发送短信给客户手机实现类
         try {
             // 大于发送短信, Map来自ActiveMQ 生成者
-            SmsUtil.sendMessages( mapMessage.getString("code"), mapMessage.getString("telephone") );
-            System.out.println( "-----发送消息成功..."+mapMessage.getString("code"));
+            SmsUtil.sendMessages(mapMessage.getString("code"), mapMessage.getString("telephone"));
+            System.out.println("-----发送消息成功..." + mapMessage.getString("code"));
         } catch (Exception e) {//JMS
             e.printStackTrace();
         }
