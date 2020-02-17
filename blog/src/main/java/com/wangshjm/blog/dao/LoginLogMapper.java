43627b369bd53c3bedd29d9db45cae1266cecb76
WangShuaijie <2283613941@qq.com>
2020-02-17 15:00:05 +0800

    dao层更新

--- blog/src/main/java/com/wangshjm/blog/dao/LoginLogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wangshjm.blog.dao;
-
-import com.wangshjm.blog.entity.LoginLog;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface LoginLogMapper extends Mapper<LoginLog> {
-}
--- blog/src/main/java/com/wangshjm/blog/dao/RoleUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wangshjm.blog.dao;
-
-import com.wangshjm.blog.entity.RoleUser;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface RoleUserMapper extends Mapper<RoleUser> {
-}
